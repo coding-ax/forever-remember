216615bb38163fb8d1572d87c119d8edc881335c
Hhhzj-7 <aalehuangzhijian@163.com>
2020-04-20 16:38:58 +0800

    h01

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import store from './store'
 /**饿了么UI */
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
+import echarts from 'echarts'
+Vue.prototype.$echarts = echarts
+
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
